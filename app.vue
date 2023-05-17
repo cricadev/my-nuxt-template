36ad12e06745080004080c760062ae05773af24b
Cristian Casallas <cristiancasallas998@gmail.com>
2023-05-17 08:53:00 -0500

    tailwind, color and devtools

--- app.vue
+++ app.vue
@@ -1,3 +1,3 @@
 <template>
-  <HelloWorld />
+  <ToggleTheme/>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,28 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    modules: [
+    '@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxt/devtools',
+
+  ],
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '',
+    storageKey: 'nuxt-color-mode'
+  },
+    // Defaults options
+    tailwindcss: {
+      cssPath: '~/assets/css/tailwind.css',
+      configPath: 'tailwind.config',
+      exposeConfig: false,
+      exposeLevel: 2,
+      injectPosition: 'first',
+      viewer: true,
+    },
+
+
 })
